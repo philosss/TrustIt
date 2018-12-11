bbaf63ba626faed321eaac69d5893eaca4f6c946
Boy Raaijmakers <b.a.j.raaijmakers@student.tue.nl>
2018-12-11 11:40:33 +0100

    Changes in the mobile app

--- ebay-app/src/shared/baseUrl.ts
+++ ebay-app/src/shared/baseUrl.ts
@@ -1,1 +1,1 @@
-export const baseUrl = "http://127.0.0.1:3001/"; // Port 3001 because 3000 is already in use by the Composer REST API
+export const baseUrl = "http://65.52.159.90:3001/"; // Port 3001 because 3000 is already in use by the Composer REST API
